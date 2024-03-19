72a722b9239ac440ed7a36110f2c3f18a92a4f3f
mehebul02 <mehebul2123@gmail.com>
2024-03-19 21:07:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='text-center'>Awsome Nabver</h1>
+     
+    </>
+  )
+}
+
+export default App
