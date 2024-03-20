a6e902e665673709fdd0150ce7b7d877e724c9ef
mehebul02 <mehebul2123@gmail.com>
2024-03-20 15:34:12 +0600

    navigate add

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,17 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post =({post})=> {
     const {id,title,body} =post
+    const navigate =useNavigate()
     const postStyle ={
         border:'2px solid red',
         padding:'20px',
     borderRadius:'10px'
+    }
+    const handleMoreDetails =()=>{
+        navigate(`/post/${id}`);
+
     }
     return(
         <div style={postStyle}>
             <p className="text-4xl">Post OF Id:{id}</p>
             <p className="text-2xl">Title:{title}</p>
             <Link to={`/post/${id}`}><button className="btn">Post Details</button></Link>
+            <button className="btn bg-green-700" onClick={handleMoreDetails}>See more details</button>
 
         </div>
     )
--- src/components/post/postDetails/PostDetails.jsx
+++ src/components/post/postDetails/PostDetails.jsx
@@ -1,13 +1,20 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetails =()=>{
     const post = useLoaderData()
     const {id,title,body} =post
+    const navigate = useNavigate()
+    const handleGoToBack =()=>{
+        navigate(-1)
+
+
+    }
     return(
         <div className="text-center">
             <h1 className="text-center text-2xl ">This Post Details:{id} </h1>
             <h1>Title:{title}</h1>
             <p><small>{body}</small></p>
+            <button className="btn" onClick={handleGoToBack}>Go To Back</button>
 
         </div>
     )
